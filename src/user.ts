5262d8b9c42322dfdc7fcf5971896a39b2314959
GjoeKariuki <githaigageorge96@gmail.com>
2023-05-08 00:23:35 +0300

    fetched project details

--- src/user.ts
+++ src/user.ts
@@ -1,5 +1,33 @@
-const logOut= document.querySelector('#logout')! as HTMLDivElement
+interface iProjects {
+    projectname: string,
+    projectdescription: string,
+    assigneduser: string,
+    status: string,
+    id: number
+}
 
-logOut.addEventListener('click', () => {
-    window.location.href = '../login.html';
-});
+async function getProjectDetails() 
+{
+    const response = await fetch("http://localhost:3000/Projects")
+    const projdetails = await response.json() as iProjects[]
+    const projdivcont = document.querySelector("#projdivcont") as HTMLDivElement;
+    let html = '';
+    for(let i=0;i<projdetails.length;i++){
+        if(projdetails[i]){
+            html += `
+            <details>
+                <summary id="projdetails">
+                    <p>
+                        <span>${projdetails[i].projectname}</span>
+                        <span id="projdoer">${projdetails[i].assigneduser}</span>
+                        <span id="projstatus">${projdetails[i].status}</span>
+                    </p>
+                </summary>
+                ${projdetails[i].projectdescription}
+            </details>
+            
+            `
+        }
+    }
+
+}
