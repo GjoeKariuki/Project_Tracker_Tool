502f949f31b7ab8c35cd67d3f9821b9407246007
GjoeKariuki <githaigageorge96@gmail.com>
2023-05-08 00:41:48 +0300

    fetched project details into each section

--- src/user.ts
+++ src/user.ts
@@ -11,10 +11,29 @@ async function getProjectDetails()
     const response = await fetch("http://localhost:3000/Projects")
     const projdetails = await response.json() as iProjects[]
     const projdivcont = document.querySelector("#projdivcont") as HTMLDivElement;
+    const assigndivcont = document.querySelector("#assigndivcont") as HTMLDivElement;
+
     let html = '';
+    let assignhtml = ''
     for(let i=0;i<projdetails.length;i++){
         if(projdetails[i]){
             html += `
+            <details>
+                <summary>
+                <p>
+                    <span>Project 1</span>
+                    <span>${projdetails[i].assigneduser}</span>
+                    <span id="pending"> Pending 
+                        <img src="./images/done.png" id="pending-img" alt="icon">
+                        <img src="./images/arrow-down.png" id="pending-img" alt=""></p>
+                        <!-- formatted span tag correctly -->
+                    </span>
+                </summary>
+                ${projdetails[i].projectdescription}
+            </details>  `
+        }
+        if(projdetails[i].status == "completed"){
+            assignhtml += `
             <details>
                 <summary id="projdetails">
                     <p>
@@ -24,10 +43,12 @@ async function getProjectDetails()
                     </p>
                 </summary>
                 ${projdetails[i].projectdescription}
-            </details>`
+            </details>
+            `            
         }
     }
-    projdivcont.innerHTML = html
+    assigndivcont.innerHTML = html;
+    projdivcont.innerHTML = assignhtml;  
 
 }
 
